7a253cd478973cbcbceb9ac6cdf8bf32e04ec716
thatFarhan <fabdillahmu@gmail.com>
2024-01-16 16:04:06 +0800

    Added: Guru mengajar hari ini

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\JadwalModel;
 use App\Models\MapelModel;
+use App\Models\GuruModel;
 use Illuminate\Http\Request;
 
 class JadwalController extends Controller
@@ -16,46 +17,63 @@ class JadwalController extends Controller
         $jadwalXA = JadwalModel::with('mapel')
         ->where('hari', '=', date('D'))
         ->where('kelas', '=', 'A')
+        ->where('angkatan', '=', 10)
         ->orderBy('jam_pelajaran', 'asc')
         ->get();
 
         $jadwalXB = JadwalModel::with('mapel')
         ->where('hari', '=', date('D'))
         ->where('kelas', '=', 'B')
+        ->where('angkatan', '=', 10)
         ->orderBy('jam_pelajaran', 'asc')
         ->get();
 
         $jadwalXC = JadwalModel::with('mapel')
         ->where('hari', '=', date('D'))
         ->where('kelas', '=', 'C')
+        ->where('angkatan', '=', 10)
         ->orderBy('jam_pelajaran', 'asc')
         ->get();
 
         $jadwalXD = JadwalModel::with('mapel')
         ->where('hari', '=', date('D'))
         ->where('kelas', '=', 'D')
+        ->where('angkatan', '=', 10)
         ->orderBy('jam_pelajaran', 'asc')
         ->get();
 
         $jadwalXE = JadwalModel::with('mapel')
         ->where('hari', '=', date('D'))
         ->where('kelas', '=', 'E')
+        ->where('angkatan', '=', 10)
         ->orderBy('jam_pelajaran', 'asc')
         ->get();
 
         $jadwalXF = JadwalModel::with('mapel')
         ->where('hari', '=', date('D'))
         ->where('kelas', '=', 'F')
+        ->where('angkatan', '=', 10)
         ->orderBy('jam_pelajaran', 'asc')
         ->get();
 
         $jadwalXG = JadwalModel::with('mapel')
         ->where('hari', '=', date('D'))
         ->where('kelas', '=', 'G')
+        ->where('angkatan', '=', 10)
         ->orderBy('jam_pelajaran', 'asc')
         ->get();
 
-        return view('jadwal', compact('jadwalXA', 'jadwalXB', 'jadwalXC', 'jadwalXD', 'jadwalXE', 'jadwalXF', 'jadwalXG'));
+        $guruToday = GuruModel::whereIn('id_guru', function($query){
+            $query->select('id_guru')
+            ->from(with(new MapelModel)->getTable())
+            ->whereIn('id_mapel', function($query){
+                $query->select('id_mapel')
+                ->from(with(new JadwalModel)->getTable())
+                ->where('hari', '=', date('D'));
+            });
+        })->get();
+
+        return view('jadwal', compact('jadwalXA', 'jadwalXB', 'jadwalXC', 'jadwalXD', 'jadwalXE', 'jadwalXF', 'jadwalXG', 'guruToday'));
     }
 
     /**
--- resources/views/jadwal.blade.php
+++ resources/views/jadwal.blade.php
@@ -7,69 +7,72 @@
     @vite('resources/css/app.css')
     <title>Document</title>
 </head>
-<body class="font-inter">
-    <h1>{{date("D")}}</h1>
-    <table class="table-auto border-collapse border-black m-10 text-center">
+<body class="font-inter p-10">
+    <table class="table-auto border-collapse border-black text-center">
         <thead>
             <tr>
-                <th class="border-2 border-slate-400"></th>
-                <th class="border-2 border-slate-400">1</th>
-                <th class="border-2 border-slate-400">2</th>
-                <th class="border-2 border-slate-400">3</th>
-                <th class="border-2 border-slate-400">4</th>
-                <th class="border-2 border-slate-400">5</th>
-                <th class="border-2 border-slate-400">6</th>
-                <th class="border-2 border-slate-400">7</th>
-                <th class="border-2 border-slate-400">8</th>
-                <th class="border-2 border-slate-400">9</th>
-                <th class="border-2 border-slate-400">10</th>
+                <th class="border border-slate-400"></th>
+                <th class="border border-slate-400">1</th>
+                <th class="border border-slate-400">2</th>
+                <th class="border border-slate-400">3</th>
+                <th class="border border-slate-400">4</th>
+                <th class="border border-slate-400">5</th>
+                <th class="border border-slate-400">6</th>
+                <th class="border border-slate-400">7</th>
+                <th class="border border-slate-400">8</th>
+                <th class="border border-slate-400">9</th>
+                <th class="border border-slate-400">10</th>
             </tr>
         </thead>
 
         <tbody>
             <tr>
-                <td class="border-2 border-slate-400 px-5 py-2">XA</td>
+                <td class="border border-slate-400 px-5 py-2">XA</td>
                 @foreach ($jadwalXA as $d)
-                    <td class="border-2 border-slate-400 px-5 py-2" style="background-color: {{$d->mapel->guru->warna}}">{{$d->mapel->nama_mapel}}</td>
+                    <td class="border border-slate-400 px-5 py-2" style="background-color: {{$d->mapel->guru->warna}}">{{$d->mapel->nama_mapel}}</td>
                 @endforeach
             </tr>
             <tr>
-                <td class="border-2 border-slate-400 px-5 py-2">XB</td>
+                <td class="border border-slate-400 px-5 py-2">XB</td>
                 @foreach ($jadwalXB as $d)
-                    <td class="border-2 border-slate-400 px-5 py-2" style="background-color: {{$d->mapel->guru->warna}}">{{$d->mapel->nama_mapel}}</td>
+                    <td class="border border-slate-400 px-5 py-2" style="background-color: {{$d->mapel->guru->warna}}">{{$d->mapel->nama_mapel}}</td>
                 @endforeach
             </tr>
             <tr>
-                <td class="border-2 border-slate-400 px-5 py-2">XC</td>
+                <td class="border border-slate-400 px-5 py-2">XC</td>
                 @foreach ($jadwalXC as $d)
-                    <td class="border-2 border-slate-400 px-5 py-2" style="background-color: {{$d->mapel->guru->warna}}">{{$d->mapel->nama_mapel}}</td>
+                    <td class="border border-slate-400 px-5 py-2" style="background-color: {{$d->mapel->guru->warna}}">{{$d->mapel->nama_mapel}}</td>
                 @endforeach
             </tr>
             <tr>
-                <td class="border-2 border-slate-400 px-5 py-2">XD</td>
+                <td class="border border-slate-400 px-5 py-2">XD</td>
                 @foreach ($jadwalXD as $d)
-                    <td class="border-2 border-slate-400 px-5 py-2" style="background-color: {{$d->mapel->guru->warna}}">{{$d->mapel->nama_mapel}}</td>
+                    <td class="border border-slate-400 px-5 py-2" style="background-color: {{$d->mapel->guru->warna}}">{{$d->mapel->nama_mapel}}</td>
                 @endforeach
             </tr>
             <tr>
-                <td class="border-2 border-slate-400 px-5 py-2">XE</td>
+                <td class="border border-slate-400 px-5 py-2">XE</td>
                 @foreach ($jadwalXE as $d)
-                    <td class="border-2 border-slate-400 px-5 py-2" style="background-color: {{$d->mapel->guru->warna}}">{{$d->mapel->nama_mapel}}</td>
+                    <td class="border border-slate-400 px-5 py-2" style="background-color: {{$d->mapel->guru->warna}}">{{$d->mapel->nama_mapel}}</td>
                 @endforeach
             </tr>
             <tr>
-                <td class="border-2 border-slate-400 px-5 py-2">XF</td>
+                <td class="border border-slate-400 px-5 py-2">XF</td>
                 @foreach ($jadwalXF as $d)
-                    <td class="border-2 border-slate-400 px-5 py-2" style="background-color: {{$d->mapel->guru->warna}}">{{$d->mapel->nama_mapel}}</td>
+                    <td class="border border-slate-400 px-5 py-2" style="background-color: {{$d->mapel->guru->warna}}">{{$d->mapel->nama_mapel}}</td>
                 @endforeach
             </tr>
             <tr>
-                <td class="border-2 border-slate-400 px-5 py-2">XG</td>
+                <td class="border border-slate-400 px-5 py-2">XG</td>
                 @foreach ($jadwalXG as $d)
-                    <td class="border-2 border-slate-400 px-5 py-2" style="background-color: {{$d->mapel->guru->warna}}">{{$d->mapel->nama_mapel}}</td>
+                    <td class="border border-slate-400 px-5 py-2" style="background-color: {{$d->mapel->guru->warna}}">{{$d->mapel->nama_mapel}}</td>
                 @endforeach
             </tr>
         </tbody>
     </table>
+    <br>
+    @foreach ($guruToday as $d)
+        <p>{{$d->nama_guru}}</p>
+    @endforeach
 </body>
 </html>
